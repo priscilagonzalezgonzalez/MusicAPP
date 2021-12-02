25533f6c6c4381f1da81cecf127aa343d978bc36
Peter Arámbula Vega <oscar.arambula4388@alumnos.udg.mx>
2021-12-02 00:03:31 -0600

    Vista artista ahora muestra sus álbumes

--- musicapp/src/app/pages/artist/artist.page.ts
+++ musicapp/src/app/pages/artist/artist.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ArtistasService } from 'src/app/api/artistas.service'
+import { AlbumesService } from 'src/app/api/albumes.service'
  
 @Component({
   selector: 'app-artist',
@@ -11,16 +12,22 @@ export class ArtistPage implements OnInit {
   id:any
   prevURL:string
   artista:any
+  albums:any[]
+  numAlbums:any
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    public api: ArtistasService 
+    public artistasService: ArtistasService,
+    public albumsService: AlbumesService,
+    private router: Router
   ) { }
 
   async ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id')
     this.prevURL = localStorage.getItem("URL");
-    this.artista = this.api.artista.subscribe(artista => {
+
+    // Consulta datos del artista
+    this.artista = this.artistasService.artista.subscribe(artista => {
       this.artista = artista
       if (artista.imagen == null) {
         artista.imagen = "https://www.pngitem.com/pimgs/m/146-1460913_singers-png-free-singer-icon-png-transparent-png.png"
@@ -29,8 +36,17 @@ export class ArtistPage implements OnInit {
         artista.biografia = "No hay biografia por el momento"
       }
     })
-    this.api.getArtistaById(this.id)
-    console.log(this.artista)
+    this.artistasService.getArtistaById(this.id)
+
+    // Consulta albumes artista
+    this.albumsService.albumsArtista.subscribe(albums => {
+      this.albums = albums
+      this.numAlbums = albums.length
+    })
+    this.albumsService.getAlbumsArtista(this.id)
   }
 
+  snapURL(){
+    localStorage.setItem("URL", this.router.url);
+  }
 }
